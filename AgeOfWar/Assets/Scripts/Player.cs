2fb5853f63c633332d23f0f5a240d61c1b9f044f
AdemBenJabria <adem.bj@gmail.com>
2021-01-19 19:57:21 +0100

    Start coding

---
+++ AgeOfWar/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float velocity = 5f;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(velocity, rb.velocity.y);
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        Debug.Log(rb.velocity.x);
+        animator.SetFloat("velocity", characterVelocity);
+    }
+}
